6df1516a3248dbf6f01dc7879fdaa21c0122bb14
Laur <laurentiu.anisie1@student.usv.ro>
2022-03-11 20:28:33 +0200

    Commit initial backend Django

---
+++ space/forms.py
@@ -0,0 +1,170 @@
+from dataclasses import fields
+from django import forms
+from django.forms import ModelForm, TextInput, EmailInput, CheckboxInput, DateInput, DateField, BooleanField
+from .models import Cladire, Office, Desk, Employee, Remote
+from django.utils.translation import ugettext_lazy as _
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field, Column
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
+
+
+class CladireForm(ModelForm):
+    class Meta:
+        model = Cladire
+        fields = ['denCladire', 'adresaCladire', 'nrEtaje']
+        labels = {
+            'denCladire': ('Name'),
+            'adresaCladire': ('Address'),
+            'nrEtaje': ('Floor num.'),
+        }
+
+    @property
+    def helper(self):
+        helper = FormHelper()
+        helper.layout = Layout(
+            # HTML('<h2>Create Building</h2>'),
+            Field('denCladire'),
+            Field('adresaCladire'),
+            Field('nrEtaje'),
+
+        )
+        return helper
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.helper = FormHelper()
+    #     self.helper.form_method = 'POST'
+
+    #     self.helper.layout = Layout(
+    #         Row(Column('denCladire')),
+    #         Row(
+    #             Column('adresaCladire'),
+    #             Column('nrEtaje')
+    #         ),
+    #         FormActions(
+    #             Submit('submit', 'Salvare'),
+    #             # Submit('cancel', 'Cancel', css_class='btn btn-danger')
+    #         )
+
+    #     )
+
+
+class OfficeForm(ModelForm):
+    class Meta:
+        model = Office
+        fields = ('nameOffice', 'floor', 'building', 'deskCount',
+                  'adminOffice', 'lengthOffice', 'widthOffice')
+        labels = {
+            'nameOffice': ('Office Name'),
+            'floor': ('Floor'),
+            'building': ('Building'),
+            'deskCount': ('Desk Count'),
+            'adminOffice': ('Administrator'),
+            'lengthOffice': ('Length'),
+            'widthOffice': ('Width')
+        }
+
+    @property
+    def helper(self):
+        helper = FormHelper()
+        helper.layout = Layout(
+            # HTML('<h2>Create Office</h2>'),
+            Field('nameOffice'),
+            Field('floor'),
+            Field('building'),
+            Field('deskCount'),
+            Field('adminOffice'),
+            Field('lengthOffice'),
+            Field('widthOffice')
+            # Submit('submit', 'Submit obiect', css_class='btn-success')
+        )
+        return helper
+
+
+class DeskForm(ModelForm):
+    class Meta:
+        model = Desk
+        fields = ('office', 'deskCount', 'adminOffice',
+                  'lengthDesk', 'widthDesk', 'isOccupied')
+        labels = {
+            'office': ('Office'),
+            'deskCount': ('Desk count'),
+            'adminOffice': ('Admin'),
+            'lengthDesk': ('Length'),
+            'widthDesk': ('Width'),
+            'isOccupied': ('Occupied')
+        }
+
+    @property
+    def helper(self):
+        helper = FormHelper()
+        helper.layout = Layout(
+            # HTML('<h2>Create Employee</h2>'),
+            Field('office'),
+            Field('deskCount'),
+            Field('adminOffice'),
+            Field('lengthDesk'),
+            Field('widthDesk'),
+            Field('isOccupied'),
+        )
+        return helper
+
+
+class EmployeeForm(ModelForm):
+    class Meta:
+        model = Employee
+        fields = ('firstName', 'lastName', 'role', 'gender',
+                  'birthDate', 'nationality', 'address', 'isActive')
+        labels = {
+            'firstName': ('First name'),
+            'lastName': ('Last name'),
+            'role': ('Role'),
+            'gender': ('Gender'),
+            'birthDate': ('Birth date'),
+            'nationality': ('nationality'),
+            'address': ('Address'),
+            'isActive': ('Active')
+        }
+
+    @property
+    def helper(self):
+        helper = FormHelper()
+        helper.layout = Layout(
+            HTML('<h2>Create Employee</h2>'),
+            Field('firstName'),
+            Field('lastName'),
+            Field('role'),
+            Field('gender'),
+            Field('birthDate'),
+            Field('nationality'),
+            Field('address'),
+            Field('isActive'),
+            Submit('submit', 'Submit obiect', css_class='btn-success')
+        )
+        return helper
+
+
+class RemoteForm(ModelForm):
+    class Meta:
+        model = Remote
+        fields = ('employee', 'startDate', 'duration', 'isApproved')
+        labels = {
+            'employee': ('Employee'),
+            'startDate': ('Start Date'),
+            'duration': ('Duration'),
+            'isApproved': ('Approved')
+        }
+
+    @property
+    def helper(self):
+        helper = FormHelper()
+        helper.layout = Layout(
+            # HTML('<h2>Create Employee</h2>'),
+            Field('employee'),
+            Field('startDate'),
+            Field('duration'),
+            Field('isApproved'),
+
+        )
+        return helper
---
+++ space/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.forms import CharField, DateTimeField
+import uuid
+
+
+class Cladire(models.Model):
+    codCladire = models.AutoField(primary_key=True)
+    denCladire = models.CharField(max_length=30)
+    # verbose_name=('Denumire') - pentru denumire
+    adresaCladire = models.CharField(max_length=200)
+    nrEtaje = models.IntegerField()
+    # nrBirouri = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.denCladire
+
+
+class Office(models.Model):
+    idOffice = models.AutoField(primary_key=True)
+    nameOffice = models.CharField(max_length=30)
+    floor = models.IntegerField(null=True, blank=True)
+    building = models.ForeignKey('Cladire', on_delete=models.CASCADE)
+    deskCount = models.IntegerField(null=True, blank=True)
+    usedDesks = models.IntegerField(null=True, blank=True)
+    adminOffice = models.CharField(max_length=30, null=True, blank=True)
+    lengthOffice = models.IntegerField(null=True, blank=True)
+    widthOffice = models.IntegerField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.nameOffice
+
+
+class Desk(models.Model):
+    idDesk = models.AutoField(primary_key=True)
+    office = models.ForeignKey('Office', on_delete=models.CASCADE)
+    deskCount = models.IntegerField(null=True, blank=True)
+    usedDesks = models.IntegerField(null=True, blank=True)
+    adminOffice = models.CharField(max_length=30, null=True, blank=True)
+    lengthDesk = models.IntegerField(null=True, blank=True)
+    widthDesk = models.IntegerField(null=True, blank=True)
+    isOccupied = models.BooleanField()
+    # occupiedBy=
+
+
+class Employee(models.Model):
+    STATUS = [
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+        ('NONBINARY', 'Nonbinary'),
+    ]
+    idEmployee = models.AutoField(primary_key=True)
+    firstName = models.CharField(max_length=50, null=False, blank=False)
+    lastName = models.CharField(max_length=50, null=False, blank=False)
+    role = models.CharField(max_length=50, null=True, blank=True)
+    gender = models.CharField(
+        max_length=20, choices=STATUS, null=True, blank=True)
+    birthDate = models.DateField(null=True, blank=True)
+    nationality = models.CharField(max_length=50, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    isActive = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.firstName + " " + self.lastName
+
+
+class Remote(models.Model):
+    idRemote = models.AutoField(primary_key=True)
+    employee = models.ForeignKey('Employee', on_delete=models.CASCADE)
+    startDate = models.DateField(null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True)
+    isApproved = models.BooleanField()
+    # approvedBy=
